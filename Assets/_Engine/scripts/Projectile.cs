f095746dd8e843f24c1a90225e099622b0f404aa
Christian <christian.flores@ucsp.edu.pe>
2019-05-12 17:19:19 -0500

    Tower Defense

---
+++ Assets/_Engine/scripts/Projectile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    protected float Animation;
+    public Vector3 PosInit;
+    public Vector3 PosEnd;
+    public GameObject spellPrefab;
+
+    //   void OnCollisionEnter(Collision col)
+    //   {
+    //       if (col.gameObject.tag == "Floor")
+    //       {
+    //       Debug.Log("Impacto contra el suelo");
+    //      }
+    //    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {   
+            Debug.Log("Impacto contra el suelo" + other.gameObject.tag);
+            GameObject spellGO = Instantiate(spellPrefab, this.transform.position, Quaternion.identity) as GameObject;
+            Destroy(spellGO, 2f);
+            Destroy(this.gameObject);
+        }
+
+    }
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PosInit = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Animation += Time.deltaTime;
+        Animation = Animation % 5f;
+        transform.position = MathParabola.Parabola(PosInit, PosEnd, 1f, Animation / 4f);
+        if(transform.position.y < -5.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/_Engine/scripts/Spell.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour
+{
+    public int impact;
+    public GameObject impactParticleEffect;
+    public float radiusEffect;
+
+    public int manaCost;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        GameObject particle = Instantiate(impactParticleEffect, transform.position, Quaternion.identity) as GameObject;
+        Destroy(particle, 2);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(" Colision detectada con " + other.gameObject.name);
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<Enemy>().damage(-impact);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
